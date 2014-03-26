97f4c74bd82cc4b16c30df7729b0a8a6b438ea48
Stephen Price <sprice@monsooncommerce.com>
2014-03-25 20:49:16 -0700

    rearrange

--- start.sh
+++ start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+if [ -n "${SECRET_KEY}" ]; then
+      sed -E -i "s/^environment = (.*)$/environment = \1,SECRET_KEY='$SECRET_KEY'/" /etc/supervisord.conf
+fi
 ssh-keygen -q -t rsa -f /etc/ssh/ssh_host_rsa_key -C '' -N ''
 ssh-keygen -q -t dsa -f /etc/ssh/ssh_host_dsa_key -C '' -N ''
 ssh-keygen -q -t rsa1 -f /etc/ssh/ssh_host_key -C '' -N ''
-/usr/sbin/sshd -o UseDNS=no -o UsePAM=no
 
 /usr/bin/supervisord
+/usr/sbin/sshd -D -o UseDNS=no -o UsePAM=no
